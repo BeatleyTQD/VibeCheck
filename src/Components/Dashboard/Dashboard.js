3d24caf075770e8cc3784fc321aa9236e94ceb4a
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-07-27 15:53:05 -0500

    navbar working

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,10 +1,51 @@
 import React, {useEffect, useState} from 'react';
+import { Jumbotron, Button, CardDeck, Card } from 'react-bootstrap';
 import APIManager from '../Modules/APIManager';
 import './Dashboard.css';
 
 const Dashboard = props => {
     return (
-        <h1>Hello World</h1>
+        <>
+        <Jumbotron>
+            <CardDeck>
+                <Card>
+                    <Card.Body>
+                    <Card.Title>Welcome, {sessionStorage.activeUser}.</Card.Title>
+                    <Card.Text>
+                        <ul>
+                            <li>A photo will go here</li>
+                            <li>Number of tracks will go here also</li>
+                        </ul>
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card>
+                    <Card.Body>
+                    <Card.Title>Vibe Check:</Card.Title>
+                    <Card.Text>
+                        <ul>
+                            <li>Red</li>
+                            <li>Blue</li>
+                            <li>Yellow</li>
+                            <li>Orange</li>
+                            <li>Green</li>
+                            <li>Purple</li>
+                            <li>Brown</li>
+                            <li>White</li>
+                            <li>Black</li>
+                        </ul>
+                    </Card.Text>
+                    </Card.Body>
+                </Card>
+            </CardDeck>
+        </Jumbotron>
+        <p>Stretch goal cards will go here...eventually</p>
+        <p>Stretch goal cards will go here...eventually</p>
+        <p>Stretch goal cards will go here...eventually</p>
+        <p>Stretch goal cards will go here...eventually</p>
+        <p>Stretch goal cards will go here...eventually</p>
+        <p>Stretch goal cards will go here...eventually</p>
+        </>
     )
 }
 
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import { Nav } from 'react-bootstrap';
 import './NavBar.css';
 
 const NavBar = props => {
@@ -8,34 +8,33 @@ const NavBar = props => {
         props.clearUser();
     };
     return (
-        <header>
-            <nav className="navBar">
-                <ul className="container">
-                    <li>
-                        <NavLink className="nav-link" exact to="/Dashboard" >
-                            Dashboard
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" exact to="/Library" >
-                            Library
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink className="nav-link" exact to="/Playlist" >
-                            Playlist
-                        </NavLink>
-                    </li>
-                {props.hasUser
-                    ? <li>
-                        <span className="nav-link" onClick={handleLogout}> Logout </span>
-                    </li>
-                    : <li>
-                        <NavLink className="nav-link" to="/">Login</NavLink>
-                    </li>}
-                </ul>
-            </nav>
-        </header>
+        <Nav justify variant="tabs" defaultActiveKey="/Dashboard">
+        <Nav.Item>
+            <Nav.Link href="/Dashboard">Dashboard</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-1">Library</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link eventKey="link-2">Playlists</Nav.Link>
+        </Nav.Item>
+        {props.hasUser
+            ? <Nav.Item><Nav.Link onClick={handleLogout}>Logout</Nav.Link></Nav.Item>
+            : <Nav.Item><Nav.Link href="/">Login</Nav.Link></Nav.Item>
+        }
+
+        </Nav>
+
+        //         {props.hasUser
+        //             ? <li>
+        //                 <span className="nav-link" onClick={handleLogout}> Logout </span>
+        //             </li>
+        //             : <li>
+        //                 <NavLink className="nav-link" to="/">Login</NavLink>
+        //             </li>}
+        //         </ul>
+        //     </nav>
+        // </header>
     )
 
 }
